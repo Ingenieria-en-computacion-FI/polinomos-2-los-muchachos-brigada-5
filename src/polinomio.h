5f0cbb5e5050cc22a56ea0b32ccc911e096c5608
ehidanchavez67 <ehidanchavez@unam.edu>
2026-05-12 05:03:46 +0000

    Se definieron exitosamente estructuras relacionadas a las operaciones básicas de los polinomios

---
+++ src/polinomio.h
@@ -0,0 +1,36 @@
+#ifndef __POLINOMIO_H__
+#define __POLINOMIO_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Termino {
+    float coeficiente;
+    int exponente;
+} Term;
+
+typedef struct Nodo {
+    Term termino;
+    struct Nodo* siguiente;
+    struct Nodo* anterior;
+} Nodo;
+
+typedef struct {
+    Nodo* cabeza;
+    Nodo* cola;
+    int numTerminos;
+} Polinomio;
+
+Polinomio* crearPolinomio();
+void insertarTermino(Polinomio* p, float coeficiente, int exponente);
+void eliminarTermino(Polinomio* p, int exponente);
+float potencia(float base, int exp);
+float evaluarPolinomio(Polinomio* p, float x);
+Polinomio* sumarPolinomios(Polinomio* p1, Polinomio* p2);
+Polinomio* multiplicarPolinomios(Polinomio* p1, Polinomio* p2);
+void mostrarPolinomio(Polinomio* p);
+char* polinomioToString(Polinomio* p);
+void destruirPolinomio(Polinomio* p);
+
+#endif
